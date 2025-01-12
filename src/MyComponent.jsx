4ff4f50ef09c0ccfec055b60c7e7908a74f0b6b8
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-12 18:09:32 +0600

    Event Listener

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -5,11 +5,13 @@ function MyComponent() {
     const [width, setWidth] = useState(window.innerWidth);
     const [height, setHeight] = useState(window.innerHeight);
 
+    addEventListener('resize', handleResize);
+
     function handleResize() {
         setWidth(window.innerWidth);
         setHeight(window.innerHeight);
     }
-    
+
     return (
         <div>
             <p>Window Width: {width}px</p>
