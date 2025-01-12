36a13e7110e46ff68f0c1e862fa3002fadd0a339
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-12 18:08:31 +0600

    handleResize function

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -5,6 +5,11 @@ function MyComponent() {
     const [width, setWidth] = useState(window.innerWidth);
     const [height, setHeight] = useState(window.innerHeight);
 
+    function handleResize() {
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
+    }
+    
     return (
         <div>
             <p>Window Width: {width}px</p>
